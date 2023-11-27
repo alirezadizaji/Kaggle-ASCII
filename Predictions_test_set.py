96a95b1ed5acdd0599b6b1e4adecb8123d8d7435
Michelpayan <41550779+Michelpayan@users.noreply.github.com>
2023-11-27 19:50:26 +0100

    Add files via upload

---
+++ Predictions_test_set.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+
+def make_predictions_test(df, apply_model, dictionary_letters):
+    
+    first = np.array(df.iloc[:,1:784+1])
+    second = np.array(df.iloc[:,784+1:])
+    
+    first = first.reshape(first.shape[0], 28, 28, 1).astype('float32')
+    second = second.reshape(second.shape[0], 28, 28, 1).astype('float32')
+    
+    first /= 255
+    second /= 255
+    
+    #Predictions for image A
+    y_pred_1 = [np.argmax(i) for i in apply_model.predict(first)]
+    y_pred_1 = pd.Series(y_pred_1).map(dictionary_letters)
+    y_pred_1_ascii = np.array([ord(i) for i in y_pred_1])
+    
+    #Predictions for image B
+    y_pred_2 = [np.argmax(i) for i in apply_model.predict(second)]
+    y_pred_2 = pd.Series(y_pred_2).map(dictionary_letters)
+    y_pred_2_ascii = np.array([ord(i) for i in y_pred_2])
+    
+    #Sum of both ASCII
+    summa = y_pred_1_ascii + y_pred_2_ascii
+
+    for i in range(len(summa)):
+        while summa[i] >= 122:
+            summa[i] -= 65 
+        else:
+            summa[i] = summa[i]
+
+    y_pred = np.array([chr(i) for i in summa])
+    
+    
+    df=df[["id"]]
+    df.loc[:,"label"] = y_pred
+
+    
+    return df
+
