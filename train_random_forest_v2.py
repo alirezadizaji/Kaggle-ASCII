9e266726b63d8fcf29391c53cee1778bd6e2a2a5
Alireza Dizaji <gil-estel@alirezas-mbp.sf.umontreal.ca>
2023-12-01 11:22:19 -0500

    Update: random forest

---
+++ train_random_forest_v2.py
@@ -0,0 +1,68 @@
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from Predictions_test_set import make_predictions_test
+
+from stdout_stderr_setter import stdout_stderr_setter
+# from random_forest.random_forest import RandomForestClassifier
+
+@stdout_stderr_setter("./Consoles_random_forest_v2")
+def main():
+     #Import training and test sets
+    train=pd.read_csv(os.path.join(sys.argv[1], "sign_mnist_train.csv"))
+    test = pd.read_csv(os.path.join(sys.argv[1], "test.csv"))
+
+    #Create dictionary to map the predicted numbers (0-25) (and no cases for 9=J or 25=Z because of gesture motions).
+    map_letters=pd.read_excel(os.path.join(sys.argv[1], "Letters.xlsx"))
+    map_letters["label"] = np.where(map_letters["label"] >= 9, map_letters["label"] - 1, map_letters["label"])
+    dictionary_letters = dict(map_letters.values)
+    
+    x_train, x_val,y_train, y_val = train_test_split(train.iloc[:,1:] , train["label"],
+                                                  stratify=train["label"], random_state=0)
+
+    # Reshape the data to be suitable for CNN
+    x_train=np.array(x_train).astype(np.float32)
+    x_val=np.array(x_val).astype(np.float32)
+
+    #LETS ADJUST THE NUMBER OF CLASSES SINCE 9 IS NOT INCLUDED
+    y_train=np.array(y_train)
+    y_train = np.where(y_train >= 9, y_train - 1, y_train)
+    y_val=np.array(y_val)
+    y_val = np.where(y_val >= 9, y_val - 1, y_val)
+
+    # Normalize pixel values to be between 0 and 1
+    x_train /= 255.0
+    x_val /= 255.0
+
+    hyperparameters = [
+       {"n_estimators": 50, "max_depth": 20},
+    ]
+
+    best_h: dict = None
+    best_val_acc: float = -np.inf
+
+    # for h in hyperparameters:
+    #     rf = RandomForestClassifier(**h)
+    #     rf.fit(x_train, y_train)
+    #     pred_val = rf.predict(x_val).argmax(1)
+    #     val_acc = (pred_val == y_val).sum() / y_val.size
+    #     print(f"@@ Random Forest Classifer: val acc {val_acc}, params {h}", flush=True)
+    #     if val_acc > best_val_acc:
+    #         print(f"@@@ BEST VAL ACCURACY HAS JUST CHANGED @@@", flush=True)
+    #         best_h = h
+    #         best_val_acc = val_acc
+    
+    model = RandomForestClassifier(**hyperparameters[0])
+    X = np.vstack((x_train, x_val))
+    y = np.concatenate((y_train, y_val))
+    model.fit(X, y)
+    final_pred=make_predictions_test(test,model,dictionary_letters, to_img_form=False)
+    final_pred.to_csv("rfc_first_trial.csv",index=False)
+
+
+if __name__ == "__main__":
+   main()
